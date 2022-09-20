5578fbf4cd1e30be2595d90e78e973206dceba6c
Haeun <haeun.song@mail.utoronto.ca>
2022-09-20 17:23:07 -0400

    Responsive design edited

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ prevButton.addEventListener("click", () => {
     const slideWidth = slide.clientWidth;
     slidesContainer.scrollLeft -= slideWidth;
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerButton = document.querySelector('#hamburger');
+    const menuCloseButton = document.querySelector('#menuClose')
+    const menu = document.querySelector('.slideOutNav');
+
+    hamburgerButton.addEventListener('click', (e) => {
+        menu.classList.add('open');
+    });
+    menuCloseButton.addEventListener('click', (e) => {
+        menu.classList.remove('open');
+    });
+})
